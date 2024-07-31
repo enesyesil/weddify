77ae58529ce5406dce18ff9cd332a47d5f3d4394
Enes Yesil <90360687+enesyesil@users.noreply.github.com>
2024-07-31 02:22:11 -0400

    calendar update

--- weddify/src/components/Footer.tsx
+++ weddify/src/components/Footer.tsx
@@ -8,7 +8,7 @@ const Footer: React.FC = () => {
           <p>&copy; 2024 Gaye & Enes Wedding</p>
         </div>
         <a
-            href="https://www.google.com/calendar/render?action=TEMPLATE&text=Gaye+%26+Enes+Wedding&details=Join+us+for+our+wedding+celebration&location=1811+Albion+Rd%2C+Etobicoke%2C+ON+M9W+5W4&dates=20240819T193000Z%2F20240819T223000Z"
+            href="https://calendar.google.com/calendar/u/0/r/eventedit?text=Gaye+%26+Enes+Wedding&details=Join+us+for+our+wedding+celebration&location=1811+Albion+Rd,+Etobicoke,+ON+M9W+5W4&dates=20240819T193000Z/20240819T223000Z"
             target="_blank"
             rel="noopener noreferrer"
             className="bg-blue-700 hover:bg-blue-600 text-white font-bold py-2 px-4 border-b-4 border-blue-900 hover:border-blue-700 rounded"
