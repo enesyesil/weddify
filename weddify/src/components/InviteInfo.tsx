f3eb0bee12a3ee51ec31f4e6991ec9f69e3ce83e
Enes Yesil <90360687+enesyesil@users.noreply.github.com>
2024-07-31 01:37:05 -0400

    metatags added + footer edit

--- weddify/src/components/InviteInfo.tsx
+++ weddify/src/components/InviteInfo.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import { useState } from 'react';
+import Head from 'next/head';
 import Navbar from '../components/Navbar';
 import CountdownTimer from '../components/CountdownTimer';
 import InviteForm from '../components/InviteForm';
@@ -32,22 +33,39 @@ const InvitePage: React.FC = () => {
   const currentMessage = messages[currentLanguage];
 
   return (
-    <div className="min-h-screen flex flex-col bg-amber-200 p-4">
-      <Navbar />
-      <div className="flex flex-col justify-center items-center flex-grow">
-        <CountdownTimer targetDate="2024-08-19T19:30:00" />
-        <LanguageSwitcher currentLanguage={currentLanguage} onLanguageChange={handleLanguageChange} />
-        <div className="bg-yellow-400 text-black text-center font-bold p-6 mt-6 mb-4 border-2 border-b-4 border-black rounded-lg shadow-lg max-w-lg mx-auto">
-          {currentMessage.main}
+    <>
+      <Head>
+        <title>Gaye & Enes Wedding Invitation</title>
+        <meta name="description" content="Join us in celebrating the wedding of Gaye & Enes on 19th August 2024. RSVP to let us know how many will be attending." />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta property="og:title" content="Gaye & Enes Wedding Invitation" />
+        <meta property="og:description" content="Join us in celebrating the wedding of Gaye & Enes on 19th August 2024. RSVP to let us know how many will be attending." />
+        <meta property="og:image" content="/Users/Enes/Documents/GitHub/weddify/weddify/public/wedding-background.PNG" />
+        <meta property="og:url" content="https://weddify-ten.vercel.app/" />
+        <meta property="og:type" content="website" />
+        <meta property="og:locale" content={currentLanguage === 'en' ? 'en_US' : 'tr_TR'} />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content="Gaye & Enes Wedding Invitation" />
+        <meta name="twitter:description" content="Join us in celebrating the wedding of Gaye & Enes on 19th August 2024. RSVP to let us know how many will be attending." />
+        <meta name="twitter:image" content="/Users/Enes/Documents/GitHub/weddify/weddify/public/wedding-background.PNG" />
+      </Head>
+      <div className="min-h-screen flex flex-col bg-amber-200 p-4">
+        <Navbar />
+        <div className="flex flex-col justify-center items-center flex-grow">
+          <CountdownTimer targetDate="2024-08-19T19:30:00" />
+          <LanguageSwitcher currentLanguage={currentLanguage} onLanguageChange={handleLanguageChange} />
+          <div className="bg-yellow-400 text-black text-center font-bold p-6 mt-6 mb-4 border-2 border-b-4 border-black rounded-lg shadow-lg max-w-lg mx-auto">
+            {currentMessage.main}
+          </div>
+          <div className="bg-yellow-400 text-black text-center p-4 mt-4 mb-8 border-2 border-black border-b-4 rounded-lg shadow-lg max-w-lg mx-auto">
+            <p className="text-md font-bold">{currentMessage.date}</p>
+            <p className="text-md font-bold">{currentMessage.location}</p>
+          </div>
+          <InviteForm />
         </div>
-        <div className="bg-yellow-400 text-black text-center p-4 mt-4 mb-8 border-2 border-black border-b-4 rounded-lg shadow-lg max-w-lg mx-auto">
-          <p className="text-md font-bold">{currentMessage.date}</p>
-          <p className="text-md font-bold">{currentMessage.location}</p>
-        </div>
-        <InviteForm />
+        <Footer />
       </div>
-      <Footer />
-    </div>
+    </>
   );
 };
 
